a6d107d475272a7674a550b8800c67645cb7542b
qxxd <ohqixiang@gmail.com>
2026-08-17 23:16:32 +0800

    Implemented Deadline class

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -1,2 +1,13 @@
-public class Deadline {
+public class Deadline extends Task {
+    private String by;
+
+    Deadline(String description, String by) {
+        super(description);
+        this.by = by;
+    }
+
+    @Override
+    public String toString() {
+        return "[D]" + super.toString() + " (by: " + by + ")";
+    }
 }
---
+++ src/main/java/Todo.java
@@ -0,0 +1,6 @@
+public class Todo extends Task{
+    Todo(String description) {
+        super(description);
+    }
+
+}
