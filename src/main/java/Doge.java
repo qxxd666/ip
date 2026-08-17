c645a7ccc78929cf1e8ec782e81981a6fb45b996
qxxd <ohqixiang@gmail.com>
2026-08-17 22:06:45 +0800

    Updated final variables to all caps

--- src/main/java/Doge.java
+++ src/main/java/Doge.java
@@ -2,18 +2,18 @@ import java.util.Scanner;
 
 public class Doge {
     public static void main(String[] args) {
-        final String banner = "       ____   ___   ____ _____\n" +
+        final String BANNER = "       ____   ___   ____ _____\n" +
                 "      |  _ \\ / _ \\ / ___| ____|\n" +
                 "      | | | | | | | |  _|  _|\n" +
                 "      | |_| | |_| | |_| | |___\n" +
                 "      |____/ \\___/ \\____|_____|";
-        final String separator = "    ____________________________________________________________";
+        final String SEPARATOR = "    ____________________________________________________________";
 
-        System.out.println(separator);
-        System.out.println(banner);
+        System.out.println(SEPARATOR);
+        System.out.println(BANNER);
         System.out.println("    Hello! I'm Doge.");
         System.out.println("    What can I do for you?");
-        System.out.println(separator);
+        System.out.println(SEPARATOR);
 
         Scanner scanner = new Scanner(System.in);
 
@@ -21,16 +21,15 @@ public class Doge {
             System.out.println();
             String text = scanner.nextLine();
             if (text.equals("bye")) {
-                System.out.println(separator);
+                System.out.println(SEPARATOR);
                 System.out.println("    Bye. Hope to see you again soon!");
-                System.out.println(separator);
+                System.out.println(SEPARATOR);
                 break;
             } else {
-                System.out.println(separator);
-                System.out.println("    " + text);
-                System.out.println(separator);
+                System.out.println(SEPARATOR);
+                System.out.println("    added: " + text);
+                System.out.println(SEPARATOR);
             }
         }
-
     }
 }
