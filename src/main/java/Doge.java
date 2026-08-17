de6795906cddde0685702e106abfb07cc7003323
qxxd <ohqixiang@gmail.com>
2026-08-17 21:10:17 +0800

    Renamed Duke to Doge

---
+++ src/main/java/Doge.java
@@ -0,0 +1,10 @@
+public class Doge {
+    public static void main(String[] args) {
+        String banner = "   ____   ___   ____ _____\n" +
+                "  |  _ \\ / _ \\ / ___| ____|\n" +
+                "  | | | | | | | |  _|  _|\n" +
+                "  | |_| | |_| | |_| | |___\n" +
+                "  |____/ \\___/ \\____|_____|";
+        System.out.println(banner);
+    }
+}
