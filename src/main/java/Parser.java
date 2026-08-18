3610411fbd33be400a161f752181fc6c5a4cc20e
qxxd <ohqixiang@gmail.com>
2026-08-18 14:32:53 +0800

    Modified parseDeadline and parseEvent static method to throw DogeException when format does not fit requirement

--- src/main/java/Parser.java
+++ src/main/java/Parser.java
@@ -28,14 +28,26 @@ public class Parser {
         return new Todo(description);
     }
 
-    private static Deadline parseDeadline(String description) {
+    private static Deadline parseDeadline(String description) throws DogeException {
         String[] parts = description.split("\\s+/by\\s+", 2);
+        if (parts.length != 2 || parts[0].isBlank() || parts[1].isBlank()) {
+            throw new DogeException("Use this format: deadline DESCRIPTION /by DATE");
+        }
         return new Deadline(parts[0], parts[1]);
     }
 
-    private static Event parseEvent(String description) {
+    private static Event parseEvent(String description) throws DogeException {
         String[] fromParts = description.split("\\s+/from\\s+", 2);
+        if (fromParts.length != 2 || fromParts[0].isBlank() || fromParts[1].isBlank()) {
+            throw new DogeException("Use this format: event DESCRIPTION /from START /to END");
+        }
+
         String[] toParts = fromParts[1].split("\\s+/to\\s+", 2);
+
+        if (toParts.length != 2 || toParts[0].isBlank() || toParts[1].isBlank()) {
+            throw new DogeException("Use this format: event DESCRIPTION /from START /to END");
+        }
+
         return new Event(fromParts[0], toParts[0], toParts[1]);
     }
 }
